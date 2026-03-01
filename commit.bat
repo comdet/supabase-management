5574e76b51d9ffda6da922be5e7e8ead2cc4a631
Khomdet Phueadphut <listzone@hotmail.com>
2026-03-01 11:20:55 +0700

    chore: release version v2.1.1

--- commit.bat
+++ commit.bat
@@ -1,8 +1,9 @@
 @echo off
+call npm run build
+if %errorlevel% neq 0 exit /b %errorlevel%
 git add .
-git commit -m "chore: release version v2.1.0"
-git tag -d v2.1.0
-git tag v2.1.0
+git commit -m "chore: release version v2.1.1"
+git tag v2.1.1
 git push origin main
-git push origin v2.1.0 -f
+git push origin v2.1.1
 del commit.bat
