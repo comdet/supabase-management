6701a580b4a8023f5fe35ba4ce74f66617af659a
comdet <listzone@hotmail.com>
2026-03-02 03:13:21 +0700

    remove unwanted file

--- commit.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call npm run build
-if %errorlevel% neq 0 exit /b %errorlevel%
-git add .
-git commit -m "chore: release version v2.1.1"
-git tag v2.1.1
-git push origin main
-git push origin v2.1.1
-del commit.bat
